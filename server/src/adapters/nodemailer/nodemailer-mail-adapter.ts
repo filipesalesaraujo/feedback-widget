fca928ee7e21fe6b0c619f1877f86fd5ca4a2b8e
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-06-02 19:29:46 -0300

    stage 03

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "dbd18a4748e833",
+    pass: "b78808188dd62b",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Filipe Sales <filipesalesaraujo@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
